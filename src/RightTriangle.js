e3dc752c20e268a2480d0681aece8a811672e7b4
Paulio <pauliorandall@gmail.com>
2023-11-11 21:16:45 +0000

    Add more functions

--- src/RightTriangle.js
+++ src/RightTriangle.js
@@ -1,19 +1,65 @@
 export default class RightTriangle {
-	constructor(dp = 1) {
-		this.dp = dp
-
+	constructor() {
+		// Length values
 		this.a = 3
 		this.b = 4
 		this.c = 5
 
+		// Squared values
 		this.as = 9
 		this.bs = 16
 		this.cs = 25
+
+		this._precision = 1
+	}
+
+	// setA sets the length of a then recalculates c while keeping b fixed.
+	setA(a) {
+		a = this._round(a)
+
+		if (a === this.a) {
+			return this
+		}
+
+		const oldAs = this.as
+
+		this.as = a * a
+		this.a = a
+
+		this.cs = this.as + this.bs
+		this.c = this._sqrtRound(this.cs)
+
+		return this
+	}
+
+	// setB sets the length of b then recalculates c while keeping a fixed.
+	setB(b) {
+		b = this._round(b)
+
+		if (b === this.b) {
+			return this
+		}
+
+		const oldBs = this.bs
+
+		this.bs = b * b
+		this.b = b
+
+		this.cs = this.as + this.bs
+		this.c = this._sqrtRound(this.cs)
+
+		return this
 	}
 
 	// setC sets the length of c then recalculates a and b such that they
 	// maintain their ratio.
-	setC(c) {
+	setC(c, recalc = false) {
+		c = this._round(c)
+
+		if (!recalc && c === this.c) {
+			return this
+		}
+
 		const oldCs = this.cs
 
 		this.cs = c * c
@@ -24,22 +70,77 @@ export default class RightTriangle {
 
 		this.bs = this.cs - this.as
 		this.b = this._sqrtRound(this.bs)
+
+		return this
 	}
 
-	// setA sets the length of a then recalculates c while keeping b fixed.
-	setA(a) {
-		const oldAs = this.as
+	// scaleA scales length a by the passed modifier.
+	//
+	// To clarify, a mod between 1 and -1 (exclusive) will cause length a to
+	// shrink and values beyond 1 or -1 will cause length a to grow.
+	scaleA(mod) {
+		this.setA(this.a * mod)
+		return this
+	}
 
-		this.as = a * a
-		this.a = a
+	// scaleB scales length b by the passed modifier.
+	//
+	// To clarify, a mod between 1 and -1 (exclusive) will cause length b to
+	// shrink and values beyond 1 or -1 will cause length b to grow.
+	scaleB(mod) {
+		this.setB(this.b * mod)
+		return this
+	}
 
-		this.cs = this.as + this.bs
-		this.c = this._sqrtRound(this.cs)
+	// scaleC scales length c by the passed modifier.
+	//
+	// To clarify, a mod between 1 and -1 (exclusive) will cause length c to
+	// shrink and values beyond 1 or -1 will cause length c to grow.
+	scaleC(mod) {
+		this.setC(this.c * mod)
+		return this
+	}
+
+	// precision sets the max number of decimal places values can have.
+	//
+	// After setting the precision all length values will be re-evaluated based
+	// on a new rounded value of length c. If the current precision of any length
+	// is greater than the new precision then the lengths will change.
+	//
+	// Pick your precision updates wisely. If you're externally caching values
+	// computed inside RightTriangle then a change in precision may cause
+	// equality tests to fail where, from a behavioural view of the system, they
+	// should succeed.
+	precision(dp) {
+		this._precision = dp
+		this.setC(this.c, true)
+		return this
+	}
+
+	// generateShapes returns an array of polygons containing the triangle and
+	// three squares to fit within the passed bounds.
+	//
+	// Polygon c's top left point will be (0,0) and all other positions derived
+	// from it. This is intended for easy use with the SVG g tag. To offset pass
+	// an array in the form [x, y] where both values are numbers.
+	generateShapes(offset = [0, 0]) {
+		// TODO
+
+		return [
+			// cs
+			// as
+			// bs
+			// triangle
+		]
+	}
+
+	_round(n) {
+		const dpMod = Math.pow(10, this._precision)
+		return Math.round(n * dpMod) / dpMod
 	}
 
 	_sqrtRound(n) {
 		n = Math.sqrt(n)
-		const precision = Math.pow(10, this.dp)
-		return Math.round(n * precision) / precision
+		return this._round(n)
 	}
 }
--- src/RightTriangle.test.js
+++ src/RightTriangle.test.js
@@ -4,31 +4,98 @@ import RightTriangle from './RightTriangle.js'
 // 45, 45, 90
 
 describe('RightTriangle', () => {
-	describe('setC', () => {
-		test('3, 4, 5 => ?, ?, 13', () => {
-			const t = new RightTriangle()
-			t.setC(13)
+	describe('As a leaf', () => {
+		describe('setA', () => {
+			test('3, 4, 5 => 4, 4, ?', () => {
+				const t = new RightTriangle()
+				t.setA(4)
 
-			expect(t.a).toEqual(7.8)
-			expect(t.b).toEqual(10.4)
+				expect(t.a).toEqual(4)
+				expect(t.b).toEqual(4)
+				expect(t.c).toEqual(5.7)
+			})
 		})
 
-		test('3, 4, 5 => ?, ?, 42', () => {
-			const t = new RightTriangle()
-			t.setC(42)
+		describe('setB', () => {
+			test('3, 4, 5 => 3, 5, ?', () => {
+				const t = new RightTriangle()
+				t.setB(5)
 
-			expect(t.a).toEqual(25.2)
-			expect(t.b).toEqual(33.6)
+				expect(t.a).toEqual(3)
+				expect(t.b).toEqual(5)
+				expect(t.c).toEqual(5.8)
+			})
 		})
-	})
 
-	describe('setA', () => {
-		test('3, 4, 5 => 7.8, ?, ?', () => {
-			const t = new RightTriangle()
-			t.setA(4)
+		describe('setC', () => {
+			test('3, 4, 5 => ?, ?, 13', () => {
+				const t = new RightTriangle()
+				t.setC(13)
 
-			expect(t.c).toEqual(5.7)
-			expect(t.b).toEqual(4)
+				expect(t.a).toEqual(7.8)
+				expect(t.b).toEqual(10.4)
+				expect(t.c).toEqual(13)
+			})
+
+			test('3, 4, 5 => ?, ?, 42', () => {
+				const t = new RightTriangle()
+				t.setC(42)
+
+				expect(t.a).toEqual(25.2)
+				expect(t.b).toEqual(33.6)
+				expect(t.c).toEqual(42)
+			})
+		})
+
+		describe('scaleA', () => {
+			test('by 2: 3, 4, 5 => 6, 4, ?', () => {
+				const t = new RightTriangle()
+				t.scaleA(2)
+
+				expect(t.a).toEqual(6)
+				expect(t.b).toEqual(4)
+				expect(t.c).toEqual(7.2)
+			})
+		})
+
+		describe('scaleB', () => {
+			test('by 2: 3, 4, 5 => 3, 8, ?', () => {
+				const t = new RightTriangle()
+				t.scaleB(2)
+
+				expect(t.a).toEqual(3)
+				expect(t.b).toEqual(8)
+				expect(t.c).toEqual(8.5)
+			})
+		})
+
+		describe('scaleC', () => {
+			test('by 2: 3, 4, 5 => ?, ?, 10', () => {
+				const t = new RightTriangle()
+				t.scaleC(2)
+
+				expect(t.a).toEqual(6)
+				expect(t.b).toEqual(8)
+				expect(t.c).toEqual(10)
+			})
+		})
+
+		describe('precision', () => {
+			test('adds recalculates with correct number of decimal points', () => {
+				const t = new RightTriangle()
+				t.precision(3)
+				t.setA(4)
+
+				expect(t.a).toEqual(4)
+				expect(t.b).toEqual(4)
+				expect(t.c).toEqual(5.657)
+
+				t.precision(2)
+
+				expect(t.a).toEqual(4)
+				expect(t.b).toEqual(4)
+				expect(t.c).toEqual(5.66)
+			})
 		})
 	})
 })
