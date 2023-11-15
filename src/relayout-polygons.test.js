b26a19581e01a766054f6ed5cc210841f5de0bd4
Paulio <pauliorandall@gmail.com>
2023-11-15 19:42:03 +0000

    Add relayoutPolygons test

--- src/relayout-polygons.test.js
+++ src/relayout-polygons.test.js
@@ -1,7 +1,70 @@
 import relayoutPolygons from './relayout-polygons.js'
 
 describe('relayoutPolygons', () => {
-	test('?', () => {
-		// TODO
+	test('properly translates polygons', () => {
+		const givenPolygons = [
+			[
+				[0, 5],
+				[5, 5],
+				[5, 0],
+				[0, 0],
+			],
+			[
+				[0, 0],
+				[-10, 0],
+				[-10, -10],
+				[0, -10],
+			],
+			[
+				[0, 0],
+				[10, 0],
+				[0, 100],
+			],
+		]
+
+		const expPolygons = [
+			[
+				[10, 15],
+				[15, 15],
+				[15, 10],
+				[10, 10],
+			],
+			[
+				[10, 10],
+				[0, 10],
+				[0, 0],
+				[10, 0],
+			],
+			[
+				[10, 10],
+				[20, 10],
+				[10, 110],
+			],
+		]
+
+		relayoutPolygons(givenPolygons)
+		expect(givenPolygons).toEqual(expPolygons)
+	})
+
+	test('returns expected size', () => {
+		const givenPolygons = [
+			[
+				[0, 5],
+				[15, 5],
+				[15, 0],
+				[0, 0],
+			],
+			[
+				[0, 0],
+				[-10, 0],
+				[-10, -10],
+				[0, -10],
+			],
+		]
+
+		const expSize = [25, 15]
+
+		const actSize = relayoutPolygons(givenPolygons)
+		expect(expSize).toEqual(actSize)
 	})
 })
--- src/to-svg-model.test.js
+++ src/to-svg-model.test.js
@@ -1,111 +1,109 @@
 import toSvgModel from './to-svg-model.js'
 
-describe('generate-svg', () => {
-	describe('toSvgModel', () => {
-		test('creates simple polygon model', () => {
-			const polygons = [
-				[
-					[0, 5],
-					[5, 5],
-					[5, 0],
-					[0, 0],
-				],
-			]
+describe('toSvgModel', () => {
+	test('creates simple polygon model', () => {
+		const polygons = [
+			[
+				[0, 5],
+				[5, 5],
+				[5, 0],
+				[0, 0],
+			],
+		]
 
-			const exp = {
-				tag: 'svg',
-				namespaceURI: 'http://www.w3.org/2000/svg',
-				attributes: {
-					'xmlns:xlink': 'http://www.w3.org/1999/xlink',
-					viewBox: '0 0 5 5',
-					preserveAspectRatio: 'xMidYMid',
-				},
-				style: {
-					width: '100%',
-					height: '100%',
-				},
-				children: [
-					{
-						tag: 'g',
-						children: [
-							{
-								tag: 'polygon',
-								attributes: {
-									points: '0,5 5,5 5,0 0,0',
-								},
+		const exp = {
+			tag: 'svg',
+			namespaceURI: 'http://www.w3.org/2000/svg',
+			attributes: {
+				'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+				viewBox: '0 0 5 5',
+				preserveAspectRatio: 'xMidYMid',
+			},
+			style: {
+				width: '100%',
+				height: '100%',
+			},
+			children: [
+				{
+					tag: 'g',
+					children: [
+						{
+							tag: 'polygon',
+							attributes: {
+								points: '0,5 5,5 5,0 0,0',
 							},
-						],
-					},
-				],
-			}
-
-			const act = toSvgModel(polygons)
-			expect(act).toEqual(exp)
-		})
-
-		test('creates simple polygon model', () => {
-			const polygons = [
-				[
-					[0, 5],
-					[5, 5],
-					[5, 0],
-					[0, 0],
-				],
-				[
-					[1, 2],
-					[3, 4],
-					[5, 6],
-					[7, 8],
-					[9, 0],
-				],
-				[
-					[0, 0],
-					[100, 0],
-					[0, 100],
-				],
-			]
-
-			const exp = {
-				tag: 'svg',
-				namespaceURI: 'http://www.w3.org/2000/svg',
-				attributes: {
-					'xmlns:xlink': 'http://www.w3.org/1999/xlink',
-					viewBox: '0 0 100 100',
-					preserveAspectRatio: 'xMidYMid',
+						},
+					],
 				},
-				style: {
-					width: '100%',
-					height: '100%',
-				},
-				children: [
-					{
-						tag: 'g',
-						children: [
-							{
-								tag: 'polygon',
-								attributes: {
-									points: '0,5 5,5 5,0 0,0',
-								},
-							},
-							{
-								tag: 'polygon',
-								attributes: {
-									points: '1,2 3,4 5,6 7,8 9,0',
-								},
-							},
-							{
-								tag: 'polygon',
-								attributes: {
-									points: '0,0 100,0 0,100',
-								},
-							},
-						],
-					},
-				],
-			}
+			],
+		}
 
-			const act = toSvgModel(polygons)
-			expect(act).toEqual(exp)
-		})
+		const act = toSvgModel(polygons)
+		expect(act).toEqual(exp)
+	})
+
+	test('creates simple polygon model', () => {
+		const polygons = [
+			[
+				[0, 5],
+				[5, 5],
+				[5, 0],
+				[0, 0],
+			],
+			[
+				[1, 2],
+				[3, 4],
+				[5, 6],
+				[7, 8],
+				[9, 0],
+			],
+			[
+				[0, 0],
+				[100, 0],
+				[0, 100],
+			],
+		]
+
+		const exp = {
+			tag: 'svg',
+			namespaceURI: 'http://www.w3.org/2000/svg',
+			attributes: {
+				'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+				viewBox: '0 0 100 100',
+				preserveAspectRatio: 'xMidYMid',
+			},
+			style: {
+				width: '100%',
+				height: '100%',
+			},
+			children: [
+				{
+					tag: 'g',
+					children: [
+						{
+							tag: 'polygon',
+							attributes: {
+								points: '0,5 5,5 5,0 0,0',
+							},
+						},
+						{
+							tag: 'polygon',
+							attributes: {
+								points: '1,2 3,4 5,6 7,8 9,0',
+							},
+						},
+						{
+							tag: 'polygon',
+							attributes: {
+								points: '0,0 100,0 0,100',
+							},
+						},
+					],
+				},
+			],
+		}
+
+		const act = toSvgModel(polygons)
+		expect(act).toEqual(exp)
 	})
 })
